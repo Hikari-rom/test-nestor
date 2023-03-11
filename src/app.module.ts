ef5e7dacbdd89dea718b86d578fd8c98863392dc
Hikari-rom <romainfpro@gmail.com>
2023-03-11 16:17:31 +0100

    feat: Add config to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApartmentModule } from './apartment/apartment.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,16 @@ import { BedroomModule } from './bedroom/bedroom.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BedroomModule, ApartmentModule, UserModule],
+  imports: [BedroomModule, ApartmentModule, UserModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    port: 5432,
+    username: 'nestor',
+    password: 'root',
+    database: 'nestor',
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
