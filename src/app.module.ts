1618485599328429236a2df86a87ea4024249cca
Hikari-rom <romainfpro@gmail.com>
2023-03-11 16:27:12 +0100

    feat: use .env variables for database info

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApartmentModule } from './apartment/apartment.module';
 import { AppController } from './app.controller';
@@ -7,12 +8,12 @@ import { BedroomModule } from './bedroom/bedroom.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BedroomModule, ApartmentModule, UserModule, TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot(), BedroomModule, ApartmentModule, UserModule, TypeOrmModule.forRoot({
     type: 'postgres',
     port: 5432,
-    username: 'nestor',
-    password: 'root',
-    database: 'nestor',
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
     entities: [],
     synchronize: true,
     autoLoadEntities: true,
