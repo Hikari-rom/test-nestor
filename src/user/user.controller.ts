a3a4db3b19de6d03d5341ceb5e794a2414679956
Hikari-rom <romainfpro@gmail.com>
2023-03-13 00:18:41 +0100

    feat: add routes for UsersController & Services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { DeleteResult } from "typeorm";
+import { User } from "./user.entity";
+import { UserService } from "./user.service";
 
-@Controller()
+@Controller('users')
 export class UserController {
-    // constructor(private readonly userService: userService) { }
+    constructor(private userService: UserService) { }
 
+    @Post()
+    async create(@Body() user: User): Promise<User> {
+        return await this.userService.create(user);
+    }
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get('/:id')
+    async findOne(@Param() param): Promise<User> {
+        return await this.userService.findOneById(param.id);
+    }
+
+    @Put()
+    async update(@Body() user: User): Promise<User> {
+        return await this.userService.update(user);
+    }
+
+    @Delete('/:id')
+    async deleteById(@Param() param): Promise<DeleteResult> {
+        return await this.userService.deleteOneById(param.id);
+    }
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,32 @@
+
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column({ unique: true })
+    email: string;
+    @Column()
     first_name: string;
+    @Column()
     last_name: string;
+    @Column()
     address: string;
+    @Column()
     zipcode: string;
+    @Column()
     city: string;
+    @Column()
     country: string;
+
+    // constructor(id: number, first_name: string, last_name: string, address: string, zipcode: string, city: string, country: string) {
+    //     this.id = id;
+    //     this.first_name = first_name;
+    //     this.last_name = last_name;
+    //     this.address = address;
+    //     this.zipcode = zipcode;
+    //     this.city = city;
+    //     this.country = country;
+    // }
 }
