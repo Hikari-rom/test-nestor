3408310a2493a1f4c8cdf8bd7bb95bd96c528901
Hikari-rom <romainfpro@gmail.com>
2023-03-13 18:39:19 +0100

    feat: Update Entities to add relation between apartment and user

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Apartment } from '../apartment/apartment.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
@@ -8,46 +9,42 @@ export class User {
     @ApiProperty()
     @IsNumber()
     id: number;
+
     @Column()
     @ApiProperty()
     @IsString()
     email: string;
-    @Column()
-    @ApiProperty()
-    @IsString()
-    password: string;
+
     @Column()
     @ApiProperty()
     @IsString()
     first_name: string;
+
     @Column()
     @ApiProperty()
     @IsString()
     last_name: string;
+
     @Column()
     @ApiProperty()
     @IsString()
     address: string;
+
     @Column()
     @ApiProperty()
     @IsString()
     zipcode: string;
+
     @Column()
     @ApiProperty()
     @IsString()
     city: string;
+
     @Column()
     @ApiProperty()
     @IsString()
     country: string;
 
-    // constructor(id: number, first_name: string, last_name: string, address: string, zipcode: string, city: string, country: string) {
-    //     this.id = id;
-    //     this.first_name = first_name;
-    //     this.last_name = last_name;
-    //     this.address = address;
-    //     this.zipcode = zipcode;
-    //     this.city = city;
-    //     this.country = country;
-    // }
+    @OneToMany(() => Apartment, (apartment) => apartment.owner)
+    apartments: Apartment[];
 }
