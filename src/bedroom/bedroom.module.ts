794bb6292d06eb9d809c2579b1c9aa930e2d8248
Hikari-rom <romainfpro@gmail.com>
2023-03-13 23:07:45 +0100

    feat: add controller and service for Bedroom controller

--- src/bedroom/bedroom.module.ts
+++ src/bedroom/bedroom.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Apartment } from "../apartment/apartment.entity";
+import { ApartmentModule } from "../apartment/apartment.module";
 import { BedroomController } from "./bedroom.controller";
+import { Bedroom } from "./bedroom.entity";
 import { BedroomService } from "./bedroom.service";
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Bedroom, Apartment]), ApartmentModule],
     controllers: [BedroomController],
     providers: [BedroomService],
 })
