ab8d1dfeb0844ee2e7c3bee6a1eb14f177e37355
Hikari-rom <romainfpro@gmail.com>
2023-03-14 14:40:35 +0100

    feat: add BookingModule to appModule.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ApartmentModule } from './apartment/apartment.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BedroomModule } from './bedroom/bedroom.module';
+import { BookingModule } from './booking/booking.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BedroomModule, ApartmentModule, UserModule, TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot(), BedroomModule, ApartmentModule, UserModule, BookingModule, TypeOrmModule.forRoot({
     type: 'postgres',
     port: 5432,
     username: `${process.env.DB_USER}`,
