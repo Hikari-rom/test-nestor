638aab865615452f7463e9aaaa66be40b87b77d2
Hikari-rom <romainfpro@gmail.com>
2023-03-14 01:56:48 +0100

    feat: add relation between bedroom, booking and user

--- src/booking/booking.entity.ts
+++ src/booking/booking.entity.ts
@@ -1,10 +1,28 @@
-import { Bedroom } from "src/bedroom/bedroom.entity";
-import { User } from "src/user/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsNumber } from "class-validator";
+import { ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Bedroom } from "../bedroom/bedroom.entity";
+import { User } from "../user/user.entity";
 
 export class Booking {
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
+    @IsNumber()
     id: number;
+
+    @ApiProperty({ type: () => Bedroom })
+    @ManyToOne(() => Bedroom, (bedroom) => bedroom.bookings, { eager: true })
     bedroom: Bedroom;
+
+    @ApiProperty({ type: () => User })
+    @ManyToOne(() => User, (user) => user.bookings, { eager: true })
     user: User;
-    startDate: Date;
-    endDate: Date;
+
+    @IsDate()
+    @ApiProperty()
+    date: Date;
+
+    @IsNumber()
+    @ApiProperty()
+    price: number;
 }
--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BedroomModule } from "src/bedroom/bedroom.module";
+import { UserModule } from "src/user/user.module";
 import { BookingController } from "./booking.controller";
 import { BookingService } from "./booking.service";
 
 @Module({
-    imports: [],
+    imports: [UserModule, BedroomModule, TypeOrmModule.forFeature([BedroomModule])],
     controllers: [BookingController],
     providers: [BookingService]
 })
