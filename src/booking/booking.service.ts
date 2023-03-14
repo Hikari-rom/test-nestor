d80c84268fff8b6a5847cb2d78e8431ec1c17bad
Hikari-rom <romainfpro@gmail.com>
2023-03-14 15:08:02 +0100

    fix: Fix create doesn't check bookings before save

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,8 +1,8 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { createBookingDto } from "interfaces/createBooking.dto";
-import { BedroomService } from "src/bedroom/bedroom.service";
-import { UserService } from "src/user/user.service";
+import { createBookingDto } from "../../interfaces/createBooking.dto";
+import { BedroomService } from "../bedroom/bedroom.service";
+import { UserService } from "../user/user.service";
 import { DeleteResult, Repository } from "typeorm";
 import { Booking } from "./booking.entity";
 
@@ -34,35 +34,40 @@ export class BookingService {
         }
 
         const user = await this.userService.findOneById(partialBooking.user_id);
-
         if (user == null) {
             return "This user doesn\'t exist";
         }
+
         const start_date = new Date(partialBooking.start_date);
-        const start_date_check = { ...start_date };
+        const start_date_check = new Date(JSON.parse(JSON.stringify(partialBooking.start_date)));
         const end_date = new Date(partialBooking.end_date);
+
         for (start_date_check; start_date_check <= end_date; start_date_check.setDate(start_date_check.getDate() + 1)) {
             const checkBookingDate = await this.bookingRepository.countBy({ bedroom: bedroom, date: start_date_check });
-            if (checkBookingDate) {
+            if (checkBookingDate > 0) {
                 return "This bedroom is booked on " + start_date_check.toLocaleDateString();
             }
             const checkUserBookings = await this.bookingRepository.countBy({ user: user, date: start_date_check });
-            if (checkUserBookings) {
+            if (checkUserBookings > 0) {
                 return "You have already booked a bedroom on " + start_date_check.toLocaleDateString();
             }
         }
+
         const bookings: Booking[] = [];
         for (start_date; start_date <= end_date; start_date.setDate(start_date.getDate() + 1)) {
             bookings.push(await this.bookingRepository.create({ date: start_date, user: user, bedroom: bedroom, price: partialBooking.price }));
         }
+
         return await this.bookingRepository.save(bookings);
     }
 
     async deleteOneById(id: number): Promise<DeleteResult> {
+
         const booking = await this.bookingRepository.findOneBy({ id: id });
         if (booking == null) {
             return null;
         }
+
         return await this.bookingRepository.delete(booking);
     }
 
